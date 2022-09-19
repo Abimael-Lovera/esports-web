c410415521b555e8be667e8bb51c74c78bda8cfd
Abimael Lovera <abimaellovera@gmail.com>
2022-09-19 09:28:44 -0300

    Configurando tailwind e Pagina inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
+import './styles/main.css';
+import logoImg from './assets/logo.svg';
+
 export default function App() {
-	return <h1>Iniciando projeto NLW eSports</h1>;
+	return (
+		<div className="max-w-[1344px] mx-auto flex items-center flex-col my-20">
+			<img src={logoImg} alt="logo" />
+			<h1 className="text-6xl text-white font-black mt-20">
+				Seu duo está aqui
+			</h1>
+		</div>
+	);
 }
